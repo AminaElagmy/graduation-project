dae67d06e4b7df0ff0f562c51676a63223797ba7
AminaElagmy <aminaelagmy@gmail.com>
2023-05-06 12:32:58 +0200

    fixed migration name contact_us

--- database/migrations/2023_05_02_051812_create_Contact_us_table.php
+++ database/migrations/2023_05_02_051812_create_Contact_us_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('Contact_us', function (Blueprint $table) {
+        Schema::create('contact_us', function (Blueprint $table) {
             $table->id();
             $table-> string ('name',50)->unique();
             $table-> string ('email')->uniqe();
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Contact_us');
+        Schema::dropIfExists('contact_us');
     }
 };
