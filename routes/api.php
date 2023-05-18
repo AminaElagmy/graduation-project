71dfd6d259dfe6cb175c7ac0462c0eb848015362
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2023-05-18 14:08:00 +0300

    add  Auth User

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactUsController;
@@ -15,8 +16,14 @@ use App\Http\Controllers\ContactUsController;
 |
 */
 
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});*/
+});
 
-Route::post('/contact/create', [ContactUsController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('contact/create', [ContactUsController::class, 'store']);
+});
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
